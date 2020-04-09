e59b9d2b13f3ad7c4d35d3411d8b777a33a19529
Tom Christie <tom@tomchristie.com>
2020-04-09 16:39:20 +0100

    Package statics and templates (#9)
    * Add DataSource API
    
    * statics and templates move into the package itself

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from starlette.applications import Starlette
-from starlette.routing import Mount
+from starlette.routing import Mount, Route
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 import databases
@@ -10,8 +11,7 @@ import datetime
 
 database = databases.Database('sqlite:///test.db')
 models = orm.ModelRegistry(database=database)
-templates = Jinja2Templates(directory='templates')
-statics = StaticFiles(directory='statics')
+statics = StaticFiles(packages=['dashboard'])
 
 
 class Notes(orm.Model):
@@ -24,38 +24,15 @@ class Notes(orm.Model):
         'completed': orm.Boolean(title="Completed", default=False)
     }
 
-
-import typesystem
-import random
-import datetime
-
-
-users = dashboard.MockDataSource(
-    schema=typesystem.Schema(
-        fields={
-            "pk": typesystem.Integer(title="Identity", read_only=True, default=dashboard.autoincrement()),
-            "username": typesystem.String(title="Username", max_length=100),
-            "is_admin": typesystem.Boolean(title="Is Admin", default=False),
-            "joined": typesystem.DateTime(title="Joined", read_only=True, default=datetime.datetime.now),
-        }
-    ),
-    initial=[
-        {
-            "username": f"user{i}@example.org",
-            "is_admin": random.choice([True, False]),
-        } for i in range(123)
-    ]
-)
-
 admin = dashboard.Dashboard(tables=[
-    dashboard.DashboardTable(ident="users", title="Users", datasource=users.order_by('-pk')),
     dashboard.DashboardTable(ident="notes", title="Notes", datasource=Notes.objects.order_by('-id')),
 ])
 
 
 routes = [
     Mount("/admin", app=admin, name='dashboard'),
-    Mount("/statics", app=statics, name='static')
+    Mount("/statics", app=statics, name='static'),
+    Route("/", endpoint=RedirectResponse(url='/admin')),
 ]
 
 app = Starlette(debug=True, routes=routes, on_startup=[database.connect], on_shutdown=[database.disconnect])
